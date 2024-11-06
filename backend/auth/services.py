53fb8283b3698fd9bf5a01d2888fc467405c4d1a
DravaGen <sigulya2018@mail.ru>
2024-11-06 14:36:19 +0300

    changed jwt data from dict to schema AccessTokenData and added function check_user_min_status for requests

--- backend/auth/services.py
+++ backend/auth/services.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
 
@@ -5,7 +6,8 @@ from .jwt import JWTService
 from jwt.exceptions import DecodeError, InvalidSignatureError, \
     ExpiredSignatureError
 
-from users.schemas import UserStatus
+from .schemas import AccessTokenData
+from users.schemas import UserStatus, weights_user_status
 
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="/login")
@@ -13,11 +15,11 @@ oauth2_schema = OAuth2PasswordBearer(tokenUrl="/login")
 
 def get_token_data(
         token: str = Depends(oauth2_schema)
-) -> str:
+) -> AccessTokenData:
     """Возвращает данные из токена 'Authorization'"""
 
     try:
-        token_data = JWTService.decode(token)
+        token_data = AccessTokenData(**JWTService.decode(token))
 
     except (DecodeError, InvalidSignatureError, ExpiredSignatureError):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
@@ -26,16 +28,30 @@ def get_token_data(
 
 
 def get_user_id(
-        data: dict = Depends(get_token_data)
-) -> str:
+        data: AccessTokenData = Depends(get_token_data)
+) -> UUID:
     """Возвращает id авторизованного пользователя"""
 
-    return data["sub"]
+    return data.sub
 
 
 def get_user_status(
-        data: dict = Depends(get_token_data)
+        data: AccessTokenData = Depends(get_token_data)
 ) -> UserStatus:
     """Возвращает status авторизованного пользователя"""
 
-    return UserStatus(data["status"])
+    return data.status
+
+
+def check_user_min_status(min_status: UserStatus) -> Depends:
+    """Проверяет статус для доступа"""
+
+    def logic(user_status: UserStatus = Depends(get_user_status)) -> None:
+
+        if weights_user_status[min_status] > weights_user_status[user_status]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Not enough rights"
+            )
+
+    return Depends(logic)
