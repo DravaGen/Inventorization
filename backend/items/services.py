33db2dca573aa88d25918c91f02bf92cfe3aa174
DravaGen <sigulya2018@mail.ru>
2024-11-23 17:28:30 +0300

    added checks for not found

--- backend/items/services.py
+++ backend/items/services.py
@@ -1,4 +1,8 @@
-from .models import ItemORM
+from uuid import UUID
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import ItemORM, ItemShopORM
 from .schemas import ItemSchema, ItemResponse
 
 
@@ -32,3 +36,49 @@ def format_items_quantity(
         )
 
     return response
+
+
+async def check_item_exists(
+        item_id: UUID,
+        db: AsyncSession
+) -> bool:
+    """Возвращает True если item существует"""
+
+    return bool(await db.get(ItemORM, item_id))
+
+
+async def raise_if_item_not_exists(
+        item_id: UUID,
+        db: AsyncSession
+) -> None:
+    """Поднимает ошибку если item не существует"""
+
+    if not await check_item_exists(item_id, db):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="item not found"
+        )
+
+
+async def check_item_in_shop_exists(
+        item_id: UUID,
+        shop_id: UUID,
+        db: AsyncSession
+) -> bool:
+    """Возрощяет True если item есть в магазине"""
+
+    return bool(await db.get(ItemShopORM, (item_id, shop_id)))
+
+
+async def raise_if_item_in_shop_not_found(
+        item_id: UUID,
+        shop_id: UUID,
+        db: AsyncSession
+) -> None:
+    """Поднимает ошибку если item нет в магазине"""
+
+    if not await check_item_in_shop_exists(item_id, shop_id, db):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="item in shop not found"
+        )
