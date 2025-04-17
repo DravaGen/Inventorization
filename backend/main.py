b3038ad315a3855627bcb34210a10598d727e45f
DravaGen <sigulya2018@mail.ru>
2025-04-17 16:39:39 +0300

    Added ResponseDescription and ResponseDescription to describe the response code. ResponseOK has been rewritten, now it can accept the status code, and the openapi_restart function has been added to automatically issue response_model TextResponse for ResponseOK.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import get_type_hints
+from fastapi import FastAPI, APIRouter
+from responses import ResponseOK, TextResponse
 
 from config import Config
 from auth.handlers import auth_router
@@ -7,26 +9,43 @@ from shops.handlers import shops_router
 from items.handlers import items_router
 
 
-app = FastAPI(
-    debug=Config.DEBUG
-)
-app.include_router(
+root_router = APIRouter()
+
+root_router.include_router(
     auth_router,
     tags=["Auth"],
     include_in_schema=Config.DEBUG
 )
-app.include_router(
+root_router.include_router(
     users_router,
     prefix="/users",
     tags=["Users"]
 )
-app.include_router(
+root_router.include_router(
     shops_router,
     prefix="/shops",
     tags=["Shop"]
 )
-app.include_router(
+root_router.include_router(
     items_router,
     prefix="/items",
     tags=["Items"]
 )
+
+
+def openapi_prestart() -> None:
+
+    for route in root_router.routes:
+        if (
+            get_type_hints(
+                route.__dict__["endpoint"]
+            ).get("return") == ResponseOK
+        ):
+            setattr(route, "response_model", TextResponse)
+
+
+openapi_prestart()
+app = FastAPI(
+    debug=Config.DEBUG
+)
+app.include_router(root_router)
--- backend/responses/__init__.py
+++ backend/responses/__init__.py
@@ -1,5 +1,68 @@
-from pydantic import BaseModel
+from typing import Type, TypeVar, Optional
+from pydantic import BaseModel, model_validator
+from fastapi.responses import JSONResponse
 
 
-class ResponseOK(BaseModel):
-    detail: str = "ok"
+RESPONSE_MODEL = TypeVar("MODEL", bound=BaseModel)
+
+
+class TextResponse(BaseModel):
+    detail: str
+
+
+class ResponseOK(JSONResponse):
+
+    def __init__(
+        self,
+        detail: str,
+        status_code: int = 200,
+    ):
+        # content = BaseResponseContent(detail) if type(detail) == str else detail
+        super().__init__(
+            TextResponse(detail=detail).model_dump(),
+            status_code, None, None, None
+        )
+
+
+class ResponseDescription(BaseModel):
+    status_code: int
+    model: Optional[Type[str] | RESPONSE_MODEL] = None
+    description: Optional[str] = None
+
+    @model_validator(mode="after")
+    def validate_empty(self):
+
+        if (
+            not len(
+                self.model_dump(
+                    exclude="status_code",
+                    exclude_unset=True
+                ).values()
+            ) != 0
+        ):
+            raise ValueError(f"response code {self.status_code} not field")
+
+        if self.model == str:
+            self.model = TextResponse(detail="string")
+
+        return self
+
+
+class ResponseDescriptions(BaseModel):
+    responses: tuple[ResponseDescription, ...]
+
+
+    def forming(self) -> dict[int, dict]:
+
+        response = {}
+
+        for x in self.responses:
+            response[x.status_code] = x.model_dump(
+                exclude=["status_code", "model"],
+                exclude_unset=True
+            )
+
+            if x.model:
+                response[x.status_code]["model"] = x.model
+
+        return response
