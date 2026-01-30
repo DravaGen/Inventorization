77595c634949c0be3961472951cc22b11f44f9a9
DravaGen <sigulya2018@mail.ru>
2026-01-30 11:51:02 +0300

    the visual part for entering the frontend has been written

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
+import "./App.css"
+
 const App = () => {
   return (
-    <div>
-      <h1>Inventorization</h1>
+    <div id="handshake">
+      <div className="block">
+
+        <label>Inventorization</label>
+        <div id="login_form">
+          <input
+            type="text"
+            placeholder="Введите email"
+          />
+          <div className="inline-group">
+            <input
+              type="text"
+              placeholder="Код"
+            />
+            <button>Отправить</button>
+
+          </div>
+          <button>Войти</button>
+        </div>
+
+      </div>
     </div>
   )
 }
