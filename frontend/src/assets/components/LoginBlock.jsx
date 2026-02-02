5a1433cf4e967ba0eb344059236c43ea94409915
DravaGen <sigulya2018@mail.ru>
2026-02-02 16:38:39 +0300

    linking states in the LoginBlock component

--- frontend/src/assets/components/LoginBlock.jsx
+++ frontend/src/assets/components/LoginBlock.jsx
@@ -1,19 +1,51 @@
+import { useState } from "react"
+
 import Input from "./Input"
 import Button from "./Button"
 
 const LoginBlock = () => {
+
+    const [email, setEmail] = useState("")
+    const [code, setCode] = useState("")
+
     return (
         <div id="login-block">
             <div className="block">
 
                 <h3>Вход в Inventorization</h3>
                 <div id="login-form">
-                    <Input type={"text"} placeholder={"Введите email"} />
                     <div className="inline-group">
-                        <Input type={"text"} placeholder={"Код"} />
-                        <Button children={"Отправить"} />
+                        <Input
+                            id={"email"}
+                            type={"text"}
+                            placeholder={"Введите email"}
+                            value={email}
+                            onChange={(e) => {setEmail(e.target.value)}}
+                        />
+                        <Button
+                            children={"Отправить"}
+                            disabled={email.trim().length == 0}
+                            style={{width: "fit-conten"}}
+                            onClick={() => {
+
+                            }}
+                        />
                     </div>
-                    <Button children={"Войти"} />
+                    <div className="inline-group">
+                        <Input
+                            id={"code"}
+                            type={"text"}
+                            placeholder={"Код"}
+                            value={code}
+                            onChange={(e) => {setCode(e.target.value)}}
+                        />
+                    </div>
+                    <Button
+                        children={"Войти"}
+                        onClick={() => {
+
+                        }}
+                    />
                 </div>
 
             </div>
