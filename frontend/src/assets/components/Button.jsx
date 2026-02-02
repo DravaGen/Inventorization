8304c5d1fade79231fda33a93e3a43b5601d6de9
DravaGen <sigulya2018@mail.ru>
2026-02-02 10:55:08 +0300

    update Button companent

--- frontend/src/assets/components/Button.jsx
+++ frontend/src/assets/components/Button.jsx
@@ -1,5 +1,5 @@
-const Button = ({children, className=""}) => {
-    return <button className={className}>{children}</button>
+const Button = ({children, ...props}) => {
+    return <button {...props}>{children}</button>
 }
 
 export default Button
