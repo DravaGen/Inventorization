27663488f8add4fe3548ea87035e6040dd6f3f17
DravaGen <sigulya2018@mail.ru>
2026-02-09 12:15:11 +0300

    ManagerItem added a checkbox for selecting an item

--- frontend/src/assets/components/Manager/ManagerItem.jsx
+++ frontend/src/assets/components/Manager/ManagerItem.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Input from "../Input/Input"
 import "./index.css"
 
 
@@ -6,12 +7,15 @@ const ManagerItem = ({ title, children }) => {
     const [open, setOpen] = useState(false)
 
     return (
-        <div className={`manager-item ${open ? "open" : ""}`}>
-            <div
-                className="manager-item-header"
-                onClick={() => setOpen(!open)}
-            >{title}</div>
-            <div className="manager-item-body">{children}</div>
+        <div className="manager-item-row">
+            <Input type="checkbox" />
+            <div className={`manager-item ${open ? "open" : ""}`}>
+                <div
+                    className="manager-item-header"
+                    onClick={() => setOpen(!open)}
+                >{title}</div>
+                <div className="manager-item-body">{children}</div>
+            </div>
         </div>
     )
 }
