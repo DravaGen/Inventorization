a8c9a6601da625655e54a226f7830fe6f0285560
DravaGen <sigulya2018@mail.ru>
2026-02-27 14:30:37 +0300

    fix item.purchase_price in ManagerItem

--- frontend/src/assets/components/Manager/ManagerItemShopItem.jsx
+++ frontend/src/assets/components/Manager/ManagerItemShopItem.jsx
@@ -10,7 +10,7 @@ const ManagerItemShopItem = ({ item, isQueue = false }) => {
             <div>id: {item.id}</div>
             <div>Количество: {item.quantity}</div>
             <div>Цена продажи: {item.price}</div>
-            <div>Цена закупки: {item.price}</div>
+            <div>Цена закупки: {item.purchase_price}</div>
             {isQueue && <div className="item-in-queue">Находится в очереди</div>}
         </ManagerItem>
     )
