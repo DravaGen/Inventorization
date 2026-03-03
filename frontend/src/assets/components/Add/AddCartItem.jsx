d758679912d06709fe1144aa9407d63ed7f01548
DravaGen <sigulya2018@mail.ru>
2026-03-03 16:24:20 +0300

    feat(AddCartItem): add component

---
+++ frontend/src/assets/components/Add/AddCartItem.jsx
@@ -0,0 +1,68 @@
+import { useCallback, useRef, useState } from "react"
+
+import AddConstructor from "./AddConstructor"
+import { Button } from "../Button"
+import { InlineGroup } from "../InlineGroup"
+import { InputNumber, isUnsignedInteger } from "../Input"
+import { Select, SelectOption } from "../Select"
+import RestAPI from "../../../RestAPI"
+
+
+const AddCartItem = ({ items, shop_id, getItemsInCart }) => {
+
+    const [form, setForm] = useState({
+        itemId: false,
+        quantity: 1
+    })
+
+    const itemRef = useRef()
+    const quantityRef = useRef()
+
+    const logicAddCartItem = useCallback(async () => {
+        await RestAPI.addCartItem(shop_id, form.itemId, form.quantity)
+        itemRef.current.clear()
+        quantityRef.current.value = "1";
+        setForm({itemId: false, quantity: 1})
+        getItemsInCart()
+    }, [
+        shop_id, form, getItemsInCart,
+        itemRef, quantityRef, setForm
+    ])
+
+
+    return (
+        <AddConstructor
+            blockName={"Добавить в корзину"}
+        >
+            <InlineGroup>
+                <Select
+                    ref={itemRef}
+                    searchInput={true}
+                    visibleCount={4}
+                    updateForm={() => [setForm, "itemId"]}
+                >{items.map((item) => (
+                    <SelectOption
+                        key={item.id}
+                        value={item.id}
+                    >{item.name}</SelectOption>
+                ))}</Select>
+            </InlineGroup>
+            <InputNumber
+                ref={quantityRef}
+                type={"number"}
+                placeholder={"Количество"}
+                condition={isUnsignedInteger}
+                updateForm={() => [setForm, "quantity"]}
+                defaultValue={"1"}
+            />
+            <Button
+                disabled={!Object.values(form).every(Boolean)}
+                onClick={logicAddCartItem}
+            >Добавить</Button>
+
+        </AddConstructor>
+    )
+}
+
+
+export default AddCartItem
