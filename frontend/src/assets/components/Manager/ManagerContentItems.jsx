8264992c25bbb91187bef79dd7c6358c9ea237bf
DravaGen <sigulya2018@mail.ru>
2026-03-03 09:21:25 +0300

    feat: add search for components  in ManagerContentItems

--- frontend/src/assets/components/Manager/ManagerContentItems.jsx
+++ frontend/src/assets/components/Manager/ManagerContentItems.jsx
@@ -1,16 +1,45 @@
+import { useState, useCallback, useMemo} from "react"
+
 import ManagerContent from "./ManagerContent"
+import { SearchInput } from "../Input"
+
 
 const ManagerContentItems = ({
     elements,
     elementName,
     Component,  // eslint-disable-line no-unused-vars
     useIndexInKey=false,
+    useSearchInput=()=>[false, ""],
     setSelectedList=()=>{},
     ...props
 }) => {
+
+    const [search, setSearch] = useState("")
+    const [enableSearch, keySearch] = useSearchInput()
+
+    const showElements = useMemo(() => {
+        if (!search.trim()) return elements
+
+        return elements.filter(element => {
+            const searchValue = search.toLowerCase()
+            return (
+                element[keySearch].toLowerCase().startsWith(searchValue)
+                || (element?.id ?? "").toLowerCase().startsWith(searchValue)
+            )
+        })
+    }, [search, keySearch, elements])
+
+    const setSearchValue = useCallback((e) => {
+        setSearch(e.target.value)
+    }, [setSearch])
+
     return (
         <ManagerContent>
-            {elements.map(
+            {enableSearch && <SearchInput
+                value={search}
+                onChange={setSearchValue}
+            />}
+            {showElements.map(
                 (element, index) => {
                     const key = useIndexInKey ? `${element.id}-${index}` : element.id
                     return <Component
